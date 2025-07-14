124bd827ba0fb85dae5133a16973d1165f0d5523
nutujean <j@brzfreight.com>
2025-07-14 11:23:26 -0500

    Prima versiune Ebay-Anunturi

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Serverul rulează cu succes!');
+});
+
+app.listen(port, () => {
+  console.log(`Serverul rulează pe portul ${port}`);
+});
+
+
