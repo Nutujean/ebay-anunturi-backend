6b3e84e8dac71d8c020f3f49557b7f7f5deac92c
nutujean <j@brzfreight.com>
2025-07-14 12:41:59 -0500

    Fix port pentru Render

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-// Middleware (dacă ai nevoie)
-// app.use(express.json());
-
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
